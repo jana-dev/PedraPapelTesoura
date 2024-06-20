534ca12322898e9bb2676b9f263a5b445657a1c4
Janaina Tavares <janixgg@outlook.com>
2024-06-19 21:01:24 -0300

    Adicionado animacao nas escolhas do jogador

--- app/src/main/java/com/janatavares/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/janatavares/pedrapapeltesoura/MainActivity.java
@@ -2,6 +2,7 @@ package com.janatavares.pedrapapeltesoura;
 
 import static kotlin.random.RandomKt.Random;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,14 +11,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.os.CountDownTimer;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Objects;
 import java.util.Random;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imagePedra;
+    ImageView imagePapel;
+    ImageView imageTesoura;
+    ScaleAnimation aumentarAnimacao;
+    ScaleAnimation diminuirAnimacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +46,70 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+
+
+        //Animações
+        aumentarAnimacao = new ScaleAnimation(
+                1f, 1.2f, // Escala de 100% para 120% na largura
+                1f, 1.2f, //Escala de 100% para 120% na altura
+                Animation.RELATIVE_TO_SELF, 0.5f, //Ponto de pivo no centro horizontal
+                Animation.RELATIVE_TO_SELF, 0.5f //Ponto de pivo no centro vertical
+        );
+        aumentarAnimacao.setDuration(500); //Duração da animação em milissegundos
+
+        diminuirAnimacao = new ScaleAnimation(
+                1.2f,1f,
+                1.2f,1f,
+                Animation.RELATIVE_TO_SELF, 0.5f, //Ponto de pivo no centro horizontal
+                Animation.RELATIVE_TO_SELF, 0.5f //Ponto de pivo no centro vertical
+        );
+        diminuirAnimacao.setDuration(500); //Duração da animação em milissegundos
+
+        imagePedra = findViewById(R.id.imagePedra);
+        imagePapel = findViewById(R.id.imagePapel);
+        imageTesoura = findViewById(R.id.imageTesoura);
+
     }
 
     public void selecionarPedra(View view){
+
+        imagePedra.startAnimation(aumentarAnimacao);
+        new CountDownTimer(500, 500) {
+            public void onTick(long millisUntilFinished) {
+
+            }
+            public void onFinish() {
+                imagePedra.startAnimation(diminuirAnimacao);
+            }
+        }.start();
         verificarGanhador("pedra");
     }
 
     public void selecionarPapel(View view){
+
+        imagePapel.startAnimation(aumentarAnimacao);
+        new CountDownTimer(500, 500) {
+            public void onTick(long millisUntilFinished) {
+
+            }
+            public void onFinish() {
+                imagePapel.startAnimation(diminuirAnimacao);
+            }
+        }.start();
         verificarGanhador("papel");
     }
 
     public void selecionarTesoura(View view){
+
+        imageTesoura.startAnimation(aumentarAnimacao);
+        new CountDownTimer(500, 500) {
+            public void onTick(long millisUntilFinished) {
+
+            }
+            public void onFinish() {
+                imageTesoura.startAnimation(diminuirAnimacao);
+            }
+        }.start();
         verificarGanhador("tesoura");
     }
     private String gerarEscolhaAleatoriaApp() {
@@ -80,6 +149,5 @@ public class MainActivity extends AppCompatActivity {
             textoResultado.setText("Você perdeu!");
         }
     }
-
 
 }
